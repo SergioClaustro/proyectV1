fa2c4a9788b8790a2f05e5f67836cef6cfa6f2f1
SergioClaustro <sergio.claustro5250@alumnos.udg.mx>
2025-05-19 11:49:43 -0600

    Regresando el 200

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -38,7 +38,7 @@ app.post("/users", (req, res) => {
   res.status(201).json(newUser);
 });
 app.get("/health", (req, res) => {
-  res.status(500).send("Error");
+  res.status(200).send("Ok");
 });
 
 app.get("/profile", (req, res) => {
